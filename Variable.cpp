8ef24bef45be0273063684cce88ce682c6cbbbda
ChouYun <chowduen@gmail.com>
2017-08-27 19:17:08 +0800

    2017/8/27

---
+++ Variable.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+   // TODO:
+   // Please complete this program using variables of int, char, string and float/double
+
+
+
+   cout << "Please enter your" << endl;
+
+   cout << "Name: ";
+   
+   cout << "Age: ";
+
+   cout << "Blood Type: ";
+
+   cout << "Height(meter): ";
+
+   cout << "Weight(kg): ";
+
+   cout << "Hi! I am " << ". I am " << " years old. My blood type is " 
+        << ". I am " << " meters tall and weight " << " kilograms." << endl;
+
+   return 0;
+}
