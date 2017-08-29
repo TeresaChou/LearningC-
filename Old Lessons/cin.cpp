9875794a842c5e0284c88826a3aedb4839192199
ChouYun <chowduen@gmail.com>
2017-08-29 16:36:33 +0800

    Old lessons

---
+++ Old Lessons/cin.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+   string name;
+   cout << "Please enter your name:";
+   cin >> name;
+   cout  << "\n my name is " << name << endl;
+
+   return 0;
+
+}
