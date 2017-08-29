98784b848847b95f8ff552f42c11c5af1d457f1b
ChouYun <chowduen@gmail.com>
2017-08-29 16:27:12 +0800

    Operator_math.cpp

---
+++ Operator_math.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+   // Please complete the following two exercises.
+   // One is to claculate BMI with the given height and weight.
+   // One is to transform temperature degree from Fahrenheit to Celsius.
+   
+   cout << "Enter you weight(kg):";
+   cout << "Enter you height(meter):";
+   cout << "Your BMI is " << endl << endl;
+
+   cout << "Enter temperature degree in Fahrenheit:";
+   cout << " = " << "degrees Celsius." << endl;
+
+   return 0;
+}
