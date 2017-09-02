3214da960397c5fe0bbe7f29aa038dcc449b67dd
JonathanYu <yue801126@gmail.com>
2017-09-02 18:52:15 +0800

    Operator_bool_done

--- Operator_bool.cpp
+++ Operator_bool.cpp
@@ -5,20 +5,27 @@ int main() {
 
    // Please fill in the quote marks with 'T' or 'F'
    // according to different assigned value
-   
-   bool True = true, False = false; 
+
+   bool True = true, False = false;
 
    // This is an exmaple
    cout << !True << " = F" << endl;
 
-   // Now try it yourself!
-   cout << False || True << " = " << endl;
-   cout << !False && !!True << " = " << endl;
-   cout << 100-100 == 0 << " = " << endl;
-   cout << 15+15 != 30 << " = " << endl;
-   cout << 10 <= 45-35 << " = " << endl;
-   cout << 33 > 98 << " = " << endl;
-   cout << (45>=10) || (14<8) << " = " << endl;
+  // Now try it yourself!
+
+   cout << (False || True) << " = T " << endl;
+
+   cout << (!False && !!True) << " = T " << endl;
+
+   cout << (100-100 == 0 )<< " = T " << endl;
+
+   cout << (15+15 != 30) << " = F " << endl;
+
+   cout << (10 <= 45-35) << " = T " << endl;
+
+   cout << (33 > 98) << " = F " << endl;
+
+   cout <<((45>=10) || (14<8))<< " = T " << endl;
 
    return 0;
 }
