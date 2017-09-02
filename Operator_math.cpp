f99791c6cdf5e07473ef1bed28fb55a9add97805
ChouYun <chowduen@gmail.com>
2017-09-02 22:46:45 +0800

    correction of the homework

--- Operator_math.cpp
+++ Operator_math.cpp
@@ -19,7 +19,9 @@ int main() {
 
 
    double a = height, b = weight;
-   cout << "Your BMI is " << b/(a*a) << endl << endl;
+   cout << "Your BMI is " << b/(a*a) << endl << endl;    // 這兩行可以改成：
+   // cout << "Your BMI is " << weight/(height*height) << endl << endl;
+   // double 跟 float 都可以用來存小數，只是double可以存更精細的值
 
    float Fahrenheit;
    cout << "Enter temperature degree in Fahrenheit:";
@@ -27,6 +29,8 @@ int main() {
 
    float  c = Fahrenheit;
    cout << Fahrenheit << " Fahrenheit = " << (c-32)*5/9 << " degrees Celsius." << endl;
+   // 這兩行可以改成：
+   // cout << Fahrenheit << " Fahrenheit = " << (Fahrenheit-32)*5/9 << " degrees Celsius." << endl;
 
    return 0;
 
