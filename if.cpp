f602dabb2b340adb59df741c8f51ea31dec26660
JonathanYu <yue801126@gmail.com>
2017-09-07 23:55:06 +0800

    if_homework_done

--- if.cpp
+++ if.cpp
@@ -3,22 +3,40 @@
 using namespace std;
 
 int main() {
-   
-   // Please complete the program so that when the users input his age, 
+
+   // Please complete the program so that when the users input his age,
    // the program will show the correspond priesthood office name and duties.
 
    int age;
-   bool MP;
-   cout << "My priesthood office: ";
+   char MP;
+   char Y;
+   cout << "My priesthood office \n Plesae enter your age: ";
    cin >> age;
-   cout << "Melchizedek Priesthood given(1/0): " << endl;
-   cin >> MP;
+   if (age >= 18)
+    {
+    cout << "Melchizedek Priesthood given(Y/N): ";
+    cin >> MP;
 
-   cout << "I am a deacon/teacher/priest/elder who holds Aaronic/Melchizedek Priesthood." << endl;
-   cout << "I have the authority to";
-   cout << "pass the sacrament, baptize people, bless the sacrament, and give priesthood blessing to others." << endl;
+    if (MP == 'Y' || MP == 'y')
+    cout << "I'm an elder who holds the Melchizdek Priesthood." << endl;
+    else if (MP == 'N' || MP == 'n')
+    cout << "I'm a deacon who holds the Aaronic Priesthood.\n I have the authority to pass, prepare, bless the sacrament, and baptize people. " << endl;
 
-   cout << "Error: invalid input!" << endl;
+   }
+   else if ((age<18) && (age>=16))
+    cout << "I'm a priest who holds the Aaronic Priesthood.\n I have the authority to pass the sacrament." << endl;
+   else if ((age<16) && (age>=14))
+    cout << "I'm a teacher who holds the Aaronic Priesthood.\n I have the authority to pass and prepare the sacrament. " << endl;
+   else if ((age<14) && (age>=12))
+    cout << "I'm a deacon who holds the Aaronic Priesthood.\n I have the authority to pass, prepare, bless the sacrament, and baptize people. " << endl;
+   else if (age<12)
+    cout << "You should be prepared to receive the Aronic Preisthood!" << endl;
+   else
+     {
+        cout << "Error: invalid input!" << endl;
+     }
+
+
 
    return 0;
 }
