58ef528a40990678cd87cc23696a47806a762870
JonathanYu <yue801126@gmail.com>
2017-09-08 23:55:40 +0800

    loop1_done

--- loop1.cpp
+++ loop1.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
 
    // Please use both while loop and for loop to print out a 九九乘法表
-   int grade[8][8];
+   int a,b,c;
+   for (a=1;a<10;a++)
+   {
+    for (b=1;b<10;b++)
+     {
+       int c = a*b;
+
+        cout << a << "X" << b << "=" << c << '\t';
+     }
+       cout << endl;
+
+   }
    return 0;
 }
