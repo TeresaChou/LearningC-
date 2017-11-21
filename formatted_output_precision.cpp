553eba6dcecceaf939c4d7c16c2200adc36ed12f
JonathanYu <yue801126@gmail.com>
2017-11-22 01:12:17 +0800

    precision&sizeof_homework

--- formatted_output_precision.cpp
+++ formatted_output_precision.cpp
@@ -5,26 +5,27 @@
 #include <cstdio>
 using namespace std;
 
-// When you start doin this exercise, first build and run this file, read it carefully, 
+// When you start doin this exercise, first build and run this file, read it carefully,
 // then check for the todo part at the end of the code.
 
 int main()
 {
    double pi = M_PI;    // M_PI is a constant stored in <cmath>
 
-   // This is Part I. 
+   // This is Part I.
    // It shows you how to print out float points with different precision
    // using cout
 
    cout << "cout.precision() by default = " << cout.precision() << endl;
-   cout << "              cout : pi = " << pi << endl;
-   cout.precision(10);
-   cout << " cout.precision(10): pi = " << pi << endl;
-   cout << "   setprecision(20): pi = " << setprecision(20) << pi << endl;
+   printf("printf by default pi =%6lf\n",pi);
+   printf(" %.10lf\n",pi);
+   printf(" cout.precision(10): pi = %.10f\n", pi);
+   printf(" setprecision(20): pi = %.20f\n",pi);
    cout << "cout full precision: pi = " << setprecision(numeric_limits<double>::digits10 + 1) << pi << endl << endl;
-   cout << "numeric_limits< double >::digits10 = " << numeric_limits<double>::digits10 << endl << endl;
-
-   cout << "---------------------------------------------------" << endl << endl;
+   printf(" full precision: pi = %.15f\n",pi);
+   //cout << "numeric_limits< double >::digits10 = " << numeric_limits<double>::digits10 << endl << endl;
+   printf("numeric_limits< double >::digits10 = %d\n",numeric_limits<double>::digits10);
+   printf("---------------------------------------------------\n\n");
 
    // This is Part II.
    // By assigning PI to diffrent data type, it shows that these data types have
@@ -34,31 +35,7 @@ int main()
    // The bigger the memory is, the more precisely the floating points can be stored.
    // ( You can use sizeof() to check out their sizes.)
 
-   const float floatPI = 3.1415927;                            // float:  7 digits of precision
-   const double doublePI = 3.141592653589793;                  //double: 15 digits of precision
-   const long double longDoublePI = 3.141592653589793238; //long double: 15 digits of precision
 
-   cout.precision(6);
-   cout << setw(16) << "floatPI = " << floatPI << endl;
-   cout << setw(16) << "doublePI = " << doublePI << endl;
-   cout << setw(16) << "longDoublePI = " << longDoublePI << endl << endl;
-
-   cout << "cout.precision(10):" << endl;
-   cout.precision(10);
-   cout << setw(16) << "floatPI = " << floatPI << endl;
-   cout << setw(16) << "doublePI = " << doublePI << endl;
-   cout << setw(16) << "longDoublePI = " << longDoublePI << endl << endl;
-
-   cout << "setprecision(12):" << endl;
-   cout << setw(16) << "floatPI = " << setprecision(12) << floatPI << endl;
-   cout << setw(16) << "doublePI = " << setprecision(12) << doublePI << endl;
-   cout << setw(16) << "longDoublePI = " << setprecision(12) << longDoublePI << endl << endl;
-
-   cout << "setprecision( numeric_limits<dataType>::digits10 + 1 ):" << endl;
-   cout << setw(16) << "floatPI = " << setprecision(numeric_limits<float>::digits10 + 1) << floatPI << endl;
-   cout << setw(16) << "doublePI = " << setprecision(numeric_limits<double>::digits10 + 1) << doublePI << endl;
-   cout << setw(16) << "longDoublePI = " << setprecision(numeric_limits<long double>::digits10 + 1) << longDoublePI << endl;
-   cout << "cout.precision() by default = " << cout.precision() << endl;
 
    // TODO:
    // Now is your turn!
--- sizeof.cpp
+++ sizeof.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
 #include <cstdio>
+#include <iomanip>
 using namespace std;
 
 /***********************************
  * First, you can simple build an run this file.
  * It shows the size(bytes number) of some common used data type.
  * Then, please get familiar with how printf works,
- * and 
+ * and
  ***********************************/
 
 int main()
 {
-   printf("Size of different data types:\n\n");
+   cout<<"Size of different data types:"<<endl<<endl;
 
-   printf("%14s%4d bytes = %4d bits\n", "bool", sizeof(bool), 8*sizeof(bool));
-   printf("%14s%4d bytes = %4d bits\n", "char", sizeof(char), 8*sizeof(char));
-   printf("%14s%4d bytes = %4d bits\n", "short", sizeof(short), 8*sizeof(short));
-   printf("%14s%4d bytes = %4d bits\n", "int", sizeof(int), 8*sizeof(int));
-   printf("%14s%4d bytes = %4d bits\n", "unsigned int", sizeof(unsigned int), 8*sizeof(unsigned int));
-   printf("%14s%4d bytes = %4d bits\n", "long", sizeof(long), 8*sizeof(long));
-   printf("%14s%4d bytes = %4d bits\n", "float", sizeof(float), 8*sizeof(float));
-   printf("%14s%4d bytes = %4d bits\n", "double", sizeof(double), 8*sizeof(double));
-   printf("%14s%4d bytes = %4d bits\n", "long double", sizeof(long double), 8*sizeof(long double));
+   cout<<setw(14)<<"bool"<<setw(4)<< sizeof(bool)<<" bytes = "<<setw(4)<< 8*sizeof(bool)<<" bits"<<endl;
+   cout<<setw(14) <<"char"<<setw(4)<< sizeof(char)<<" bytes = "<<setw(4)<< 8*sizeof(char)<<" bits"<<endl;
+   cout<<setw(14) <<"short"<<setw(4)<< sizeof(short)<<" bytes = "<<setw(4)<< 8*sizeof(short)<<" bits"<<endl; //short
+   cout<<setw(14) <<"int"<<setw(4)<< sizeof(int)<<" bytes = "<<setw(4)<< 8*sizeof(int)<<" bits"<<endl; //int
+   cout<<setw(14) <<"unsigned int"<<setw(4)<< sizeof(unsigned int)<<" bytes = "<<setw(4)<< 8*sizeof(unsigned int)<<" bits"<<endl; //unsigned int
+   cout<<setw(14) <<"long"<<setw(4)<< sizeof(long)<<" bytes = "<<setw(4)<< 8*sizeof(long)<<" bits"<<endl; //long
+   cout<<setw(14) <<"float"<<setw(4)<< sizeof(float)<<" bytes = "<<setw(4)<< 8*sizeof(float)<<" bits"<<endl; //float
+   cout<<setw(14) <<"double"<<setw(4)<< sizeof(double)<<" bytes = "<<setw(4)<< 8*sizeof(double)<<" bits"<<endl; //double
+   cout<<setw(14) <<"long double"<<setw(4)<< sizeof(long double)<<" bytes = "<<setw(4)<< 8*sizeof(long double)<<" bits"<<endl; // long double
 
-   
+
    // TODO: type your code here!
 
    return 0;
