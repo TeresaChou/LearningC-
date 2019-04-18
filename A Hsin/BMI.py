4b2812701161c397db2d62209518f42615e7f75d
Chou Yun <chowduen@gmail.com>
2019-04-18 14:38:08 +0800

    BMI.py

---
+++ A Hsin/BMI.py
@@ -0,0 +1,9 @@
+# You homework is to finish a program that will:
+# 1. Ask the user for his/her name
+# 2. Ask for his/her age
+# 3. Ask for his/her height
+# 4. Ask for his/her weight
+# 5. Print out a line to tell the user his/her BMI.
+# ex: Hi, A Hsin. You are 21 years-old and your BMI is 20.1.
+# Remember to notice the user type in their information with correct unit (cm/m, kg)
+# BMI is your weight(kg) divided by the square of your height(m)
