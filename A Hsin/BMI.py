4b36cbc506d36483da17dc08c26b285d784633b5
d712002 <49588216+d712002@users.noreply.github.com>
2019-04-19 18:55:24 +0800

    Update BMI.py

--- A Hsin/BMI.py
+++ A Hsin/BMI.py
@@ -7,3 +7,15 @@
 # ex: Hi, A Hsin. You are 21 years-old and your BMI is 20.1.
 # Remember to notice the user type in their information with correct unit (cm/m, kg)
 # BMI is your weight(kg) divided by the square of your height(m)
+
+name = input("What is your name? ")
+print("Hi",name,":)")
+age = input("What is your age? ")
+print("Great! Now I'm going to help you to know your BMI.")
+input()
+height = input("Well, tell me your height.(unit:m)")
+print("Thank you. ")
+weight = input("And then what is your weight?(unit:kg) ")
+print("Good job. Now press ENTER and you'll see your BMI.")
+input()
+print(name,"your BMI is",( float(weight) / (float(height)*float(height))))
